8523c20dee3fce6cb55c38e47b72f6112881c002
powalans-Adndoid-IOS-webdev <154468968+powalans-Adndoid-IOS-webdev@users.noreply.github.com>
2024-01-01 22:46:12 +0200

    main.dart

---
+++ main.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TodoListScreen1(),
+    );
+  }
+}
+
+class TodoListScreen1 extends StatefulWidget {
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen1> with WidgetsBindingObserver {
+  List<String> _todos = [];
+  TextEditingController _todoController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTodos();
+    WidgetsBinding.instance?.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      _saveTodos();
+    }
+  }
+
+  void _loadTodos() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _todos = prefs.getStringList('todos') ?? [];
+    });
+  }
+
+  void _saveTodos() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setStringList('todos', _todos);
+  }
+
+  void _addTodo() {
+    if (_todoController.text.isNotEmpty) {
+      setState(() {
+        _todos.add(_todoController.text);
+        _saveTodos(); // Save the updated list
+        _todoController.clear();
+      });
+    }
+  }
+
+  void _removeTodo(int index) {
+    setState(() {
+      _todos.removeAt(index);
+      _saveTodos(); // Save the updated list
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('To-Do List'),
+        centerTitle: true,
+            backgroundColor: Colors.orange,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _todoController,
+                    decoration: InputDecoration(
+                      hintText: 'Enter a to-do',
+                    ),
+                  ),
+                ),
+
+
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: _addTodo,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todos.length,
+              itemBuilder: (context, index) {
+                return Dismissible(
+                  key: Key(_todos[index]),
+                  onDismissed: (direction) {
+                    _removeTodo(index);
+                  },
+                  background: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 16.0),
+                      child: Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  secondaryBackground: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 16.0),
+                      child: Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  child: ListTile(
+                    title: Text(_todos[index]),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
